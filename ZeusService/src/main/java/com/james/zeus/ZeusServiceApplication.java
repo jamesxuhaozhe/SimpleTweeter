5b8066cb250cce281ab985f85c6cf8b572d33b63
Haozhe Xu <jamesxuhaozhe@gmail.com>
2017-01-25 23:55:54 -0800

    As a developer, I would like to create a package where I can start coding the SimpleTwitter Project!

---
+++ ZeusService/src/main/java/com/james/zeus/ZeusServiceApplication.java
@@ -0,0 +1,12 @@
+package com.james.zeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeusServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeusServiceApplication.class, args);
+	}
+}
