a92274f52c220714e53d280efad3065cf766d7c0
Haozhe Xu <jamesxuhaozhe@gmail.com>
2017-01-26 23:43:26 -0800

    As a developer, I would like to add AWS external sdk dependency into my project, and also copy DynamoDBLocal into my project

---
+++ ZeusService/src/main/java/com/james/zeus/controller/TweetController.java
@@ -0,0 +1,10 @@
+package com.james.zeus.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by haozhexu on 1/26/17.
+ */
+@RestController
+public class TweetController {
+}
