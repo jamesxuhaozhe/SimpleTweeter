5584dd1759a51f9eda4daaa590ca3b44420c6142
Haozhe Xu <jamesxuhaozhe@gmail.com>
2017-01-29 17:24:13 -0800

    As a developer, I would like to create a package called AthenaService which is reponsible for user related service like authentication and account info retrieval

---
+++ AthenaService/src/main/java/com/james/athena/AthenaServiceApplication.java
@@ -0,0 +1,12 @@
+package com.james.athena;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthenaServiceApplication.class, args);
+	}
+}
---
+++ AthenaService/src/test/java/com/james/athena/AthenaServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.james.athena;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AthenaServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
